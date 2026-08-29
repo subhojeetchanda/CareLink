26a3c72b2060a8583e3e245d6412499f89b799cd
Subhojeet Chanda <subhojeetchanda18@gmail.com>
2026-08-29 21:33:43 +0530

    feat: Add env var support for Firebase service account

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const { Server } = require('socket.io');
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('./serviceAccountKey.json');
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('./serviceAccountKey.json');
+}
 
 // Initialize Firebase Admin
 initializeApp({
